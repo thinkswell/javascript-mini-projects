ef9834073d68d89729ee483f8cc751708c4cf06d
Evan Merzon <evandaniel160@yahoo.com>
2020-10-09 21:31:06 -0400

    Create Random Quote Generator Project

---
+++ RandomQuote/emerzon1/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1 className="ui header">Quote Generator</h1>
+        </header>
+    );
+}
+
+export default Header;
