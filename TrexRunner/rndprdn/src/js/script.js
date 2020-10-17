ea2a74d5faf41d13256056c6c025efd113370c5f
Rendi Pradana <rendipradana4773@gmail.com>
2020-10-17 19:13:04 +0700

    add intro and fixing minor bug

--- TrexRunner/rndprdn/src/js/script.js
+++ TrexRunner/rndprdn/src/js/script.js
@@ -1,6 +1,8 @@
 const trex = document.querySelector(".trex");
 const obstacle = document.querySelector(".obstacle");
+const intro = document.querySelector(".intro");
 let counter = 0;
+let keyCode;
 
 function jump() {
   trex.classList.add("animate");
@@ -8,6 +10,15 @@ function jump() {
     trex.classList.remove("animate");
   }, 500);
 }
+
+document.body.onkeyup = function (e) {
+  if (e.keyCode == 32) {
+    jump();
+    obstacle.style.animation = "block 1s infinite linear";
+    intro.style.display = "none";
+    keyCode = e.keyCode;
+  }
+};
 
 const checkDead = setInterval(function () {
   let trexPosition = parseInt(
@@ -20,10 +31,13 @@ const checkDead = setInterval(function () {
   if (obstacleLeft < 50 && obstacleLeft > -50 && trexPosition <= 50) {
     obstacle.style.animation = "none";
     alert("Game Over. score: " + Math.floor(counter / 100));
+    intro.style.display = "block";
     counter = 0;
-    obstacle.style.animation = "block 1s infinite linear";
+    keyCode = "";
   } else {
-    counter++;
     document.querySelector(".score").innerHTML = Math.floor(counter / 100);
+    if (keyCode == 32) {
+      counter++;
+    }
   }
 }, 10);
