a96a70260e4faf6d1e54c41928b81a970a13f3a1
Sakshi Jain <sakshi.172015@gmail.com>
2020-10-24 00:46:43 +0530

    Weather Forecast App

---
+++ WeatherForecast/sakshi172015/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styling/Header.css'
+function Header() {
+    return (
+        <header className="App-header">
+            Weather Forecast
+        </header>
+    )
+}
+
+export default Header
