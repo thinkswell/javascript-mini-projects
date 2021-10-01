599ae2b7469a6f074bd0c87bd249c8beb9a1555a
sideeq12 <abdwaheed2018@gmail.com>
2021-10-01 01:16:04 -0700

    night mode added

--- LoginPage/Rigo-Hernandez/main.js
+++ LoginPage/Rigo-Hernandez/main.js
@@ -1,6 +1,16 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+const themes = document.getElementById('icon')
+const body = document.getElementById('body')
+const sine = document.getElementById('sine')
+
+themes.addEventListener('click', ()=>{
+	console.log('clicked!!')
+	body.classList.toggle('nite')
+	sine.classList.toggle('nite1')
+
+})
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
