89e88a79a0b38a01d08ac5a9ffca5aead321056a
OmPr366 <71120474+OmPr366@users.noreply.github.com>
2021-10-05 00:27:05 +0530

    To Do App

---
+++ ToDoList/OmPr366/style.css
@@ -0,0 +1,135 @@
+@import url('https://fonts.googleapis.com/css?family=Raleway:200');
+
+body {
+    background-color: rgb(226, 225, 213);
+}
+
+.mainHead {
+    color: rgb(26, 1, 247);
+    margin-left: 30%;
+    font-family: 'Noto Serif', serif;
+}
+
+.container {
+    margin: 2rem auto;
+    border-radius: 5px;
+
+}
+
+.my-3 {
+    /* background-color: rgb(204, 200, 151); */
+    background: #ECE9E6;
+    /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #FFFFFF, #ECE9E6);
+    /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #FFFFFF, #ECE9E6);
+    /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+    background: #DAE2F8;
+    /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #D6A4A4, #DAE2F8);
+    /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #D6A4A4, #DAE2F8);
+    /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
+}
+
+.editBtn {
+    /* background-color: rgb(148, 77, 214); */
+    background: #232526;
+    /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #414345, #232526);
+    /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #414345, #232526);
+    /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    border: none;
+
+    /* margin-left: 3rem; */
+    float: right;
+}
+
+.editBtn:hover {
+    background-color: rgb(127, 6, 240);
+}
+
+.editDiv {
+    /* margin-top: auto;
+    margin-right: 0.3rem; */
+    margin: auto;
+}
+
+.editBtn {
+    margin-bottom: 0.4rem;
+}
+
+.card {
+    border-radius: 5px;
+}
+
+#mySelector {
+    width: min-content;
+    background-color: rgb(143, 196, 169);
+    margin-bottom: 0.6rem;
+    border-radius: 4px;
+}
+
+#pink {
+    background-color: rgb(195, 75, 189);
+}
+
+#Red {
+    background-color: rgb(221, 107, 107);
+}
+
+#Green {
+    background-color: rgb(17, 253, 57);
+}
+
+#yellowgreen {
+    background-color: rgb(188, 214, 118);
+}
+
+#color90 {
+    background-color: rgb(104, 170, 102);
+}
+
+
+
+.card {
+    background: #FEAC5E;
+    /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #4BC0C8, #C779D0, #FEAC5E);
+    /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #4BC0C8, #C779D0, #FEAC5E);
+    /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+}
+
+.newCard {
+    background: #abbaab;
+    background: -webkit-linear-gradient(to right, #ffffff, #abbaab);
+    /* background: linear-gradient(to right, #ffffff, #abbaab); */
+
+}
+
+
+.gradient-border {
+    border: 25px solid #B88846;
+    border-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='75' height='75'%3E%3Cg fill='none' stroke='%23B88846' stroke-width='2'%3E%3Cpath d='M1 1h73v73H1z'/%3E%3Cpath d='M8 8h59v59H8z'/%3E%3Cpath d='M8 8h16v16H8zM51 8h16v16H51zM51 51h16v16H51zM8 51h16v16H8z'/%3E%3C/g%3E%3Cg fill='%23B88846'%3E%3Ccircle cx='16' cy='16' r='2'/%3E%3Ccircle cx='59' cy='16' r='2'/%3E%3Ccircle cx='59' cy='59' r='2'/%3E%3Ccircle cx='16' cy='59' r='2'/%3E%3C/g%3E%3C/svg%3E") 25;
+}
+
+hr {
+    border: 3px dashed #1A85FD;
+}
+.allNotes{
+    margin-left: 50%;
+
+}
+.nthng{
+    margin-bottom: 2rem;
+    /* margin-left: 20%; */
+}
+h7{
+    float: right;
+}
