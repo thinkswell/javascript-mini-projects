33e964e4e7a79dd5052d51ea2d4286840fcedacb
abdulmannans <samannan1999@gmail.com>
2022-02-12 03:06:54 +0530

    Palindrome Maker with random number

--- PalindromeMaker/abdulmannans/script.js
+++ PalindromeMaker/abdulmannans/script.js
@@ -22,18 +22,26 @@ class Palindrome{
 
 
     palin(number){
+        document.getElementById("arr").style.display = "block";
+        document.getElementById("arr").innerHTML = "";
         const revnum = this.reverse(number)
 
         nums.push(number)
         breakme: if(revnum == number){
-            document.getElementById("arr").innerHTML = nums
+            nums.forEach(function(element, idx , array) {
+                if (idx === array.length - 1){
+                    var label = "<label class='label'>"+ element +"</label>";
+                }else{
+                    var label = "<label class='label'>"+ element +"</label><br> <br>";
+                }
+                document.getElementById("arr").insertAdjacentHTML("beforeend",label);
+            });
             count = 0
             nums = []
         }else{
             count++
-            if(count>=20){
-                console.log("Over the Limit")
-                document.getElementById("arr").innerHTML = nums
+            if(count>=10){
+                document.getElementById("arr").innerHTML = "Over the Limit"
                 count = 0
                 nums = []
                 break breakme
