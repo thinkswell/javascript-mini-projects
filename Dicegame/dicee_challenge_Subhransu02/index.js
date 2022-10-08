60c60f83de4c0edfb7feb0ba2ecec4876a93e3c5
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2022-10-08 13:18:09 +0530

    task completed

---
+++ Dicegame/dicee_challenge_Subhransu02/index.js
@@ -0,0 +1,26 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1 ;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource =  randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
