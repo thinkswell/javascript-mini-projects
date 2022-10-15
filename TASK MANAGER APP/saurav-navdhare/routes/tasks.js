e06662d0f5cbf368a8f7744880d548057f843c74
Saurav-Navdhare <navdharesaurav2003@gmail.com>
2022-10-15 18:59:59 +0530

    Added Task Manager App

---
+++ TASK MANAGER APP/saurav-navdhare/routes/tasks.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const {getAllTasks, getTask, createTask, updateTask, deleteTask} = require('../controllers/tasks');
+
+router.route("/")
+    .get(getAllTasks)
+    .post(createTask)
+router.route("/:id")
+    .get(getTask)
+    .patch(updateTask)
+    .delete(deleteTask);
+
+module.exports = router;
