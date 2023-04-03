e72ee1459d6ef5f33f181d61bd74312a959c396b
beginner1986 <beginner1986@gmail.com>
2023-04-03 22:04:50 +0200

    Clock hands rotation calculated

--- Clock/beginner1986/script.js
+++ Clock/beginner1986/script.js
@@ -1,4 +1,7 @@
-let clockDiv = document.querySelector('#clock');
+const clockDiv = document.querySelector('#clock');
+const hourHand = document.querySelector('#hours');
+const minuteHand = document.querySelector('#minutes');
+const secondHand = document.querySelector('#seconds');
 
 for(let number=1; number<=12; number++) {
     const numberDiv = document.createElement('div');
@@ -12,3 +15,18 @@ for(let number=1; number<=12; number++) {
     const y = -Math.cos(radians) * (clockDiv.clientHeight / 2 - 20);
     numberDiv.style.transform = `translate(${x}px, ${y}px) rotate(${angle}deg)`;
 }
+
+setInterval(() => {
+    const now = new Date();
+    const hour = now.getHours();
+    const minute = now.getMinutes();
+    const second = now.getSeconds();
+
+    const hoursRotation = 30 * (hour % 12);
+    const minutesRotation = 6 * minute;
+    const secondsRotation = 6 * second;
+
+    hourHand.style.transform = `rotate(${hoursRotation}deg)`;
+    minuteHand.style.transform = `rotate(${minutesRotation}deg)`;
+    secondHand.style.transform = `rotate(${secondsRotation}deg)`;
+}, 1000);
