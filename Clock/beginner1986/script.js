ac5ceed5e0efcd93ca965d212a2eb78cb0c2a5fd
beginner1986 <beginner1986@gmail.com>
2023-04-05 21:17:11 +0200

    Numbers position made responsive for screen resizing

--- Clock/beginner1986/script.js
+++ Clock/beginner1986/script.js
@@ -1,32 +1,53 @@
-const clockDiv = document.querySelector('#clock');
+const clockContainer = document.querySelector('#clock');
 const hourHand = document.querySelector('#hours');
 const minuteHand = document.querySelector('#minutes');
 const secondHand = document.querySelector('#seconds');
 
-for(let number=1; number<=12; number++) {
-    const numberDiv = document.createElement('div');
-    numberDiv.classList.add('number');
-    numberDiv.textContent = number;
-    clockDiv.appendChild(numberDiv);
+const placeNumbers = () => {
+    for(let number=1; number<=12; number++) {
+        const numberDiv = document.createElement('div');
+        numberDiv.classList.add('number');
+        numberDiv.textContent = number;
+        clockContainer.appendChild(numberDiv);
+        
+        const angle = number * 30;
+        const radians = angle * Math.PI / 180;
+        const offset = clockContainer.clientWidth / 2 * 0.1;
+        const x = Math.sin(radians) * (clockContainer.clientWidth / 2 - offset);
+        const y = -Math.cos(radians) * (clockContainer.clientHeight / 2 - offset);
+        numberDiv.style.transform = `translate(${x}px, ${y}px) rotate(${angle}deg)`;
+    }
+};
+
+const clearNumbers = () => {
+    const numbers = document.querySelectorAll('.number');
+
+    numbers.forEach((number => {
+        number.remove();
+    }));
+};
+
+const updateClockHands = () => {
+    setInterval(() => {
+        const now = new Date();
+        const hour = now.getHours();
+        const minute = now.getMinutes();
+        const second = now.getSeconds();
     
-    const angle = number * 30;
-    const radians = angle * Math.PI / 180;
-    const x = Math.sin(radians) * (clockDiv.clientWidth / 2 - 20);
-    const y = -Math.cos(radians) * (clockDiv.clientHeight / 2 - 20);
-    numberDiv.style.transform = `translate(${x}px, ${y}px) rotate(${angle}deg)`;
-}
+        const hoursRotation = 30 * (hour % 12);
+        const minutesRotation = 6 * minute;
+        const secondsRotation = 6 * second;
+    
+        hourHand.style.transform = `rotate(${hoursRotation}deg)`;
+        minuteHand.style.transform = `rotate(${minutesRotation}deg)`;
+        secondHand.style.transform = `rotate(${secondsRotation}deg)`;
+    }, 1000);
+};
 
-setInterval(() => {
-    const now = new Date();
-    const hour = now.getHours();
-    const minute = now.getMinutes();
-    const second = now.getSeconds();
+placeNumbers();
+updateClockHands();
 
-    const hoursRotation = 30 * (hour % 12);
-    const minutesRotation = 6 * minute;
-    const secondsRotation = 6 * second;
-
-    hourHand.style.transform = `rotate(${hoursRotation}deg)`;
-    minuteHand.style.transform = `rotate(${minutesRotation}deg)`;
-    secondHand.style.transform = `rotate(${secondsRotation}deg)`;
-}, 1000);
+window.addEventListener('resize', () => {
+    clearNumbers();
+    placeNumbers();
+});
