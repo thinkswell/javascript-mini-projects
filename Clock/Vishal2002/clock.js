4ba5660f361e7c8ce4752a9f43861ec47a78cbf7
Vishal Sharma <35897449+Vishal2002@users.noreply.github.com>
2023-10-05 19:59:00 +0530

    Updated to Camel Case from line 6 to 9 in clock.js

--- Clock/Vishal2002/clock.js
+++ Clock/Vishal2002/clock.js
@@ -4,12 +4,12 @@ const minuteHand=document.querySelector('[data-minute-hand]')
 const secondHand=document.querySelector('[data-second-hand]')
 function setclock(){
    const currentDate=new Date() 
-   const secondratio=currentDate. getSeconds()/60;
-   const minuteratio=(secondratio + currentDate.getMinutes())/60;
-   const hourratio=(minuteratio + currentDate.getHours())/12;
-   setRotation(secondHand,secondratio)
-   setRotation(minuteHand,minuteratio)
-   setRotation(hourHand,hourratio)
+   const secondRatio=currentDate. getSeconds()/60;
+   const minuteRatio=(secondRatio + currentDate.getMinutes())/60;
+   const hourRatio=(minuteRatio + currentDate.getHours())/12;
+   setRotation(secondHand,secondRatio)
+   setRotation(minuteHand,minuteRatio)
+   setRotation(hourHand,hourRatio)
 }
  
 
