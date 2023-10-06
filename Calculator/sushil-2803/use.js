69e9af8848aa3f948a92579c944ce24253b02726
BellWether <120770641+rituraj12797@users.noreply.github.com>
2023-10-06 08:10:42 +0530

    Update use.js
    some more changes were done in js due to changes in html

--- Calculator/sushil-2803/use.js
+++ Calculator/sushil-2803/use.js
@@ -1,8 +1,8 @@
 "use strict";
 
 var input = document.getElementById('input'), // input/output button
-  number = document.querySelectorAll('.numbers div'), // number buttons
-  operator = document.querySelectorAll('.operators div'), // operator buttons
+  number = document.querySelectorAll('.n'), // number buttons
+  operator = document.querySelectorAll('.o'), // operator buttons
   result = document.getElementById('result'), // equal button
   clear = document.getElementById('clear'), // clear button
   resultDisplayed = false; // flag to keep an eye on what output is displayed
