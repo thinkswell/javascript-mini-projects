4e537c5ef6bac84648f098364b7ed24df197ce5d
Aditya Garg <110886184+aditya7302@users.noreply.github.com>
2023-10-08 11:37:13 +0530

    added Digital Clock

---
+++ digital_clock/index.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const hourEl = document.getElementById("hours");
+const minuteEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+
+const updateClock = () => {
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+
+
+    if(h>12){
+        h -= 12;
+        ampm = "PM";
+    }
+
+    h = h<10 ? "0" + h: h;
+    m = m<10 ? "0" + m: m;
+    s = s<10 ? "0" + s: s;
+
+    hourEl.innerText = h;
+    minuteEl.innerText = m;
+    secondEl.innerText = s;
+    ampmEl.innerText = ampm;
+    setTimeout(() => {
+        updateClock();
+    },1000)
+};
+
+updateClock();
