cb7498f18c6247c71e58cc073370cb9732a16e67
Aditya Garg <aditya7302@gmail.com>
2023-10-08 14:21:53 +0530

    added drum kit project

---
+++ Drum/aditya7302/index.js
@@ -0,0 +1,25 @@
+const kits = ["crash" , "kick", "snare", "tom"];
+const containerEl = document.querySelector(".container");
+
+kits.forEach((kit)=>{
+    const btnEl = document.createElement("button");
+    btnEl.classList.add("btn");
+    btnEl.innerText = kit;
+    btnEl.style.backgroundImage = "url(img/"+kit+".png)";
+    containerEl.appendChild(btnEl);
+    const audioEl = document.createElement("audio");
+    audioEl.src = "sound/"+kit+".mp3";
+    containerEl.appendChild(audioEl);
+    btnEl.addEventListener("click",()=>{
+        audioEl.play();
+    });
+    window.addEventListener("keydown",(event)=>{
+        if(event.key === kit.slice(0,1)){
+            audioEl.play();
+            btnEl.style.transform = "scale(0.9)";
+            setTimeout(()=>{
+                btnEl.style.transform = "scale(1)";
+            },100)
+        }
+    })
+});
