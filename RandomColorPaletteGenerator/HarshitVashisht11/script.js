52ff708d3a0ef741e77c4e8b73e7c0b5adcae64d
Harshit Vashisht <120767685+HarshitVashisht11@users.noreply.github.com>
2023-10-08 18:12:50 +0530

    Update script.js

--- RandomColorPaletteGenerator/HarshitVashisht11/script.js
+++ RandomColorPaletteGenerator/HarshitVashisht11/script.js
@@ -1,7 +1,6 @@
 const palette = document.querySelector('.palette');
 const generateButton = document.getElementById('generateButton');
 
-// Generate a random color in hex format (#RRGGBB)
 function getRandomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
@@ -11,7 +10,6 @@ function getRandomColor() {
     return color;
 }
 
-// Create and append a color card to the palette
 function createColorCard() {
     const colorCard = document.createElement('div');
     colorCard.classList.add('color-card');
@@ -21,8 +19,7 @@ function createColorCard() {
         <div class="hex-code">${hexCode}</div>
     `;
     palette.appendChild(colorCard);
-
-    // Add click event to copy hex code to clipboard
+    
     colorCard.addEventListener('click', () => {
         const tempInput = document.createElement('input');
         tempInput.value = hexCode;
@@ -34,19 +31,16 @@ function createColorCard() {
     });
 }
 
-// Generate a random color palette with a specified number of colors
 function generatePalette(numColors) {
-    palette.innerHTML = ''; // Clear existing colors
+    palette.innerHTML = '';
     for (let i = 0; i < numColors; i++) {
         createColorCard();
     }
 }
 
-// Event listener for the "Generate Colors" button
 generateButton.addEventListener('click', () => {
     const numColors = Math.floor(Math.random() * 4 + 1) * 2 * 2; // 10, 12, 14, or 16 colors
     generatePalette(numColors);
 });
 
-// Initial palette generation
-generatePalette(10); // You can start with any initial number of colors
+generatePalette(10); 
