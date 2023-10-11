47491be226fa85df58a8669e1c1c4064c8388752
Manas Sharma <manasharma867@gmail.com>
2023-10-11 09:02:37 +0530

    modifications added

--- QuizApp/ANKUR2304/script.js
+++ QuizApp/ANKUR2304/script.js
@@ -1,125 +1,152 @@
 "use strict";
 
-const startButton = document.getElementById("start-btn");
-const nextButton = document.getElementById("next-btn");
-const questionContainerElement = document.getElementById("question-container");
-const questionElement = document.getElementById("question");
-const answerButtonsElement = document.getElementById("answer-buttons");
+const questionContainer = document.getElementById("question-container");
+const questionText = document.getElementById("question-text");
+const optionsContainer = document.getElementById("options");
+const answerFeedback = document.getElementById("answer-feedback");
+const nextButton = document.getElementById("next-button");
+const scoreValue = document.getElementById("score-value");
+const timerElement = document.getElementById("timer");
 
-let shuffledQuestions, currentQuestionIndex;
-
-startButton.addEventListener("click", startGame);
-
-nextButton.addEventListener("click", () => {
-  currentQuestionIndex++;
-  setNextQuestion();
-});
-
-function startGame() {
-  startButton.classList.add("hide");
-  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
-  currentQuestionIndex = 0;
-  questionContainerElement.classList.remove("hide");
-  setNextQuestion();
-}
-
-function setNextQuestion() {
-  resetState();
-  showQuestion(shuffledQuestions[currentQuestionIndex]);
-}
-
-function showQuestion(question) {
-  questionElement.innerText = question.question;
-  question.answers.forEach((answer) => {
-    const button = document.createElement("button");
-    button.innerText = answer.text;
-    button.classList.add("btn");
-
-    if (answer.correct) {
-      button.dataset.correct = answer.correct;
-    }
-
-    button.addEventListener("click", selectAnswer);
-    answerButtonsElement.appendChild(button);
-  });
-}
-
-function resetState() {
-  clearStatusClass(document.body);
-  nextButton.classList.add("hide");
-
-  while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
-  }
-}
-
-function selectAnswer(e) {
-  const selectedButton = e.target;
-  const correct = selectedButton.dataset.correct;
-
-  setStatusClass(document.body, correct);
-
-  Array.from(answerButtonsElement.children).forEach((button) => {
-    setStatusClass(button, button.dataset.correct);
-  });
-
-  if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove("hide");
-  } else {
-    startButton.innerText = "Restart";
-    startButton.classList.remove("hide");
-  }
-}
-
-function setStatusClass(element, correct) {
-  clearStatusClass(element);
-
-  if (correct) {
-    element.classList.add("correct");
-  } else {
-    element.classList.add("wrong");
-  }
-}
-
-function clearStatusClass(element) {
-  element.classList.remove("correct");
-  element.classList.remove("wrong");
-}
+let currentQuestionIndex = 0;
+let score = 0;
+let timer;
+let timeLeft = 20; // Initial time in seconds
 
 const questions = [
   {
     question: "What is 2 + 2?",
-    answers: [
-      { text: "4", correct: true },
-      { text: "22", correct: false },
-    ],
+    options: ["3", "4", "5", "6"],
+    answer: "4",
+    explanation: "2 + 2 equals 4.",
   },
-
   {
-    question: "Who is the best YouTuber?",
-    answers: [
-      { text: "Web Dev Simplified", correct: true },
-      { text: "Traversy Media", correct: true },
-      { text: "Dev Ed", correct: true },
-      { text: "Fun Fun Function", correct: true },
-    ],
+    question: "Which planet is known as the Red Planet?",
+    options: ["Earth", "Mars", "Jupiter", "Venus"],
+    answer: "Mars",
+    explanation: "Mars is often referred to as the Red Planet due to its reddish appearance.",
   },
-
   {
-    question: "Is web development fun?",
-    answers: [
-      { text: "Kinda", correct: false },
-      { text: "YES!!!", correct: true },
-      { text: "Um no", correct: false },
-      { text: "IDK", correct: false },
-    ],
+    question: "What is the capital of France?",
+    options: ["Berlin", "Madrid", "Paris", "Rome"],
+    answer: "Paris",
+    explanation: "The capital of France is Paris."
   },
-
   {
-    question: "What is 4 * 2?",
-    answers: [
-      { text: "6", correct: false },
-      { text: "8", correct: true },
-    ],
+    question: "Which gas do plants absorb from the atmosphere?",
+    options: ["Oxygen", "Carbon Dioxide", "Nitrogen", "Hydrogen"],
+    answer: "Carbon Dioxide",
+    explanation: "Plants absorb carbon dioxide from the atmosphere during photosynthesis."
   },
+  {
+    question: "How many continents are there on Earth?",
+    options: ["5", "6", "7", "8"],
+    answer: "7",
+    explanation: "There are seven continents on Earth: Asia, Africa, North America, South America, Antarctica, Europe, and Australia."
+  },
+  {
+    question: "What is the largest mammal on Earth?",
+    options: ["Elephant", "Blue Whale", "Giraffe", "Lion"],
+    answer: "Blue Whale",
+    explanation: "The blue whale is the largest mammal on Earth."
+  },
+  {
+    question: "Which element has the chemical symbol 'H'?",
+    options: ["Helium", "Hydrogen", "Oxygen", "Nitrogen"],
+    answer: "Hydrogen",
+    explanation: "The chemical symbol 'H' stands for hydrogen."
+  },
+  {
+    question: "Which country is known as the Land of the Rising Sun?",
+    options: ["China", "Japan", "Korea", "Thailand"],
+    answer: "Japan",
+    explanation: "Japan is known as the Land of the Rising Sun."
+  },
+  {
+    question: "What is the largest planet in our solar system?",
+    options: ["Earth", "Mars", "Jupiter", "Venus"],
+    answer: "Jupiter",
+    explanation: "Jupiter is the largest planet in our solar system."
+  },
+  {
+    question: "Which gas makes up the majority of the Earth's atmosphere?",
+    options: ["Oxygen", "Carbon Dioxide", "Nitrogen", "Hydrogen"],
+    answer: "Nitrogen",
+    explanation: "Nitrogen is the most abundant gas in the Earth's atmosphere, making up about 78% of the air we breathe."
+  },
+  // Add more questions here
 ];
+
+
+function startGame() {
+    nextButton.style.display = "none";
+    scoreValue.textContent = "0";
+    currentQuestionIndex = 0;
+    score = 0;
+    showQuestion(questions[currentQuestionIndex]);
+    startTimer();
+}
+
+function showQuestion(question) {
+    questionText.textContent = question.question;
+    optionsContainer.innerHTML = "";
+
+    question.options.forEach((option) => {
+        const optionButton = document.createElement("button");
+        optionButton.textContent = option;
+        optionButton.classList.add("option-button");
+        optionButton.addEventListener("click", () => checkAnswer(option, question.answer, question.explanation));
+        optionsContainer.appendChild(optionButton);
+    });
+}
+
+function checkAnswer(selectedOption, correctAnswer, explanation) {
+    clearInterval(timer);
+    if (selectedOption === correctAnswer) {
+        score++;
+        answerFeedback.textContent = "Correct! " + explanation;
+        answerFeedback.style.color = "green";
+    } else {
+        answerFeedback.textContent = "Wrong! " + explanation;
+        answerFeedback.style.color = "red";
+    }
+    scoreValue.textContent = score;
+    if (currentQuestionIndex < questions.length - 1) {
+        currentQuestionIndex++;
+        setTimeout(() => {
+            showQuestion(questions[currentQuestionIndex]);
+            startTimer();
+            answerFeedback.textContent = "";
+        }, 2000); // Delay before showing the next question
+    } else {
+        answerFeedback.textContent = `Quiz complete! Your score: ${score} / ${questions.length}`;
+        nextButton.style.display = "none";
+        timerElement.style.display = "none";
+    }
+}
+
+function startTimer() {
+    timeLeft = 20; // Reset the timer
+    updateTimerDisplay();
+    timer = setInterval(() => {
+        timeLeft--;
+        updateTimerDisplay();
+        if (timeLeft === 0) {
+            clearInterval(timer);
+            checkAnswer(null, questions[currentQuestionIndex].answer, questions[currentQuestionIndex].explanation);
+        }
+    }, 1000);
+}
+
+function updateTimerDisplay() {
+    timerElement.textContent = `Time Left: ${timeLeft} seconds`;
+}
+
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++;
+    showQuestion(questions[currentQuestionIndex]);
+    startTimer();
+    answerFeedback.textContent = "";
+});
+
+startGame();
