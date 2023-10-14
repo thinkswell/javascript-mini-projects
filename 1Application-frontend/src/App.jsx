dfbad91bee756f492e61169b2433f1757d059eb2
harshalhonde21 <harshalhonde17@gmail.com>
2023-10-14 17:39:24 +0530

    create the folder for frontend

---
+++ 1Application-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Fragment, useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <Fragment>
+      <h1 style={{color:'white'}}>Javascript-Mini-Project integration</h1>
+    </Fragment>
+  )
+}
+
+export default App
