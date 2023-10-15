32ff7a525c7864d93035ccd4a7956e25500e5557
harshalhonde21 <harshalhonde17@gmail.com>
2023-10-15 13:26:07 +0530

    changed

--- 1Application-frontend/src/App.jsx
+++ 1Application-frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
