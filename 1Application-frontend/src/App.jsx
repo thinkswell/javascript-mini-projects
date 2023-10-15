58ad3bc0840727aaeb6a9ec966d4c6a5a1cb1299
harshalhonde21 <harshalhonde17@gmail.com>
2023-10-15 13:31:23 +0530

    suceess

--- 1Application-frontend/src/App.jsx
+++ 1Application-frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar";
-import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar.jsx";
+import Footer from "./Components/Footer.jsx";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
