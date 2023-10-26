ef0567a817e82329ffa785c3e422306d5bec7e1a
Devanshukoli <kolidevanshu02@gmail.com>
2023-10-26 12:28:48 +0530

    final production based code.

--- FAQ_page/Devanshukoli/app.js
+++ FAQ_page/Devanshukoli/app.js
@@ -1,1 +1,7 @@
-console.log('hello world')
+const faqs = document.querySelectorAll('.faq')
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('active')
+    })
+})
