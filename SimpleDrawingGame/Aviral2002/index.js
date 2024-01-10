043cd563ae99be22e9ad7e41afd834f4d0b3049f
Aviral Srivastava <aviralsrivastava121@gmail.com>
2024-01-10 15:14:17 +0530

    First project

---
+++ SimpleDrawingGame/Aviral2002/index.js
@@ -0,0 +1,66 @@
+const canvas = document.getElementById('drawingBoard');
+const toolbar = document.getElementById('toolbar');
+const ctx = canvas.getContext('2d');
+
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
+
+canvas.width = window.innerWidth - canvasOffsetX;
+canvas.height = window.innerHeight - canvasOffsetY;
+
+let isPainting = false;
+let lineWidth = 5;
+let startX;
+let startY;
+
+toolbar.addEventListener('click', e => {
+    if (e.target.id === 'clear') {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+});
+
+toolbar.addEventListener('change', e => {
+    if(e.target.id === 'stroke') {
+        ctx.strokeStyle = e.target.value;
+    }
+
+    if(e.target.id === 'LineWidth') {
+        lineWidth = e.target.value;
+    }
+    
+});
+
+const draw = (e) => {
+    if(!isPainting) {
+        return;
+    }
+
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = 'round';
+
+    // Move to the starting position
+    ctx.moveTo(startX - canvasOffsetX, startY - canvasOffsetY);
+
+    // Draw a line to the current mouse position
+    ctx.lineTo(e.clientX - canvasOffsetX, e.clientY - canvasOffsetY);
+    ctx.stroke();
+
+    // Update the starting position for the next segment
+    startX = e.clientX;
+    startY = e.clientY;
+}
+
+canvas.addEventListener('mousedown', (e) => {
+    isPainting = true;
+    startX = e.clientX;
+    startY = e.clientY;
+});
+
+canvas.addEventListener('mouseup', e => {
+    isPainting = false;
+    // Reset the path
+    ctx.beginPath();
+});
+
+canvas.addEventListener('mousemove', draw);
+
