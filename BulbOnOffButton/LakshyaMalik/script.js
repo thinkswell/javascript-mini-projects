a72f1cefe8a529b6e652543ab66144ce39e46d2d
Hpunia1 <himanshupunia1994@gmail.com>
2024-03-28 19:08:11 -0700

    Redo the project with new images and adjust spacings

--- BulbOnOffButton/LakshyaMalik/script.js
+++ BulbOnOffButton/LakshyaMalik/script.js
@@ -3,12 +3,12 @@ const ToggleBulb = () =>{
     
     let Bulb = document.getElementById('bulbimg');
     
-    if (Bulb.src.match('./assets/bulb-on.png')){
-        Bulb.setAttribute('src','./assets/bulb-off.png');
+    if (Bulb.src.match('./assets/Bulb-on2.png')){
+        Bulb.setAttribute('src','./assets/Bulb-off2.png');
         console.log('dfghjkl');
     }
 
     else{
-        Bulb.setAttribute('src','./assets/bulb-on.png');
+        Bulb.setAttribute('src','./assets/Bulb-on2.png');
     }
 }
