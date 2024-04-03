b9cf104bb517e87900c3901e70291c3e9948e4d3
Hpunia1 <himanshupunia1994@gmail.com>
2024-04-03 14:57:08 -0700

    Add content to display message on-click

--- BulbOnOffButton/LakshyaMalik/script.js
+++ BulbOnOffButton/LakshyaMalik/script.js
@@ -6,9 +6,26 @@ const ToggleBulb = () =>{
     if (Bulb.src.match('./assets/Bulb-on2.png')){
         Bulb.setAttribute('src','./assets/Bulb-off2.png');
         console.log('dfghjkl');
+        closeDialogue();
     }
 
     else{
         Bulb.setAttribute('src','./assets/Bulb-on2.png');
+        openDialogue();
     }
+}
+const openDialogue = () => {
+    document.getElementById("dialogueBox");
+    dialogueBox.style.display = "inline-grid";
+    dialogueBox.style.position = "fixed"; // Use absolute if you want it relative to a positioned ancestor
+    dialogueBox.style.top = "60%";
+    dialogueBox.style.left = "56%";
+
+    
+}
+
+
+
+const closeDialogue = () => {
+    document.getElementById("dialogueBox").style.display = "none";
 }
