839909ee3da4afefc386b7da71640978f6f5e264
岩江蒼樹 <sokiiwae@MacBook-Pro-56.modem>
2024-11-18 18:12:17 +0900

    Make Customer review form looks like default screenshot. And I chenged to more modern JS.

---
+++ CustomerReviews/Sochan2/js/customerData.js
@@ -0,0 +1,22 @@
+
+const userObject = [
+  {
+    image: "./img/customer1.png",
+    name: "Bob Smith",
+    customerReview: "Very satisfied with the purchase."
+  },
+
+  {
+    image:  "./img/customer2.png",
+    name: "Alice Johnson",
+    customerReview: "Great service and support!"
+  },
+
+  {
+    image:  "./img/customer3.png",
+    name: "Carol White",
+    customerReview: "Could be better, but good overall."
+  }
+
+]
+
